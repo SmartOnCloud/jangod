facbc38005e1c792d526a7d0ccb41d7004862eec
Igor Mihalik <igor.mihalik@gmail.com>
2011-03-22 20:49:26 +0000

    project mavenization

--- site/upload.cmd
+++
@@ -1,4 +0,0 @@
-set GAE=D:\ide\eclipse\plugins\com.google.appengine.eclipse.sdkbundle.1.3.1_1.3.1.v201002101412\appengine-java-sdk-1.3.1\bin\
-java -cp ../bin/ WebGenerator
-call "%GAE%appcfg.cmd" update war
-pause
